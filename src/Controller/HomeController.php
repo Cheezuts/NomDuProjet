130af3cfdde1075e8cee6a7a295174b884aaead7
Cheezuts <telliercyril@gmail.com>
2023-03-24 11:08:00 +0100

    Add Template SB admin + bootstrap

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,17 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        // Chercher au niveau de la base de donnée vos Users pour pouvoir les affichers dans templates/home/index.html.twig
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'name' => 'Gourgandin',
+            'firstname' => 'Jacques'
         ]);
+    }
+
+    #[Route('/template', name: 'template')]
+    public function template(): Response
+    {
+        // Chercher au niveau de la base de donnée vos Users pour pouvoir les affichers dans templates/home/index.html.twig
+        return $this->render('template.html.twig');
     }
 }
