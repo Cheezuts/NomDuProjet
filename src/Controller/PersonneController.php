a8329a71a72513e169ec9a788d54cd65ea2b354b
Cheezuts <telliercyril@gmail.com>
2023-03-26 18:00:11 +0200

    suite et fin entity manager pour add dans la database

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PersonneController extends AbstractController
 {
     #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
+    public function addPersonne(ManagerRegistry $doctrine): Response
     {
-        return $this->render('personne/index.html.twig', [
-            'controller_name' => 'PersonneController',
+        // $this->getDoctrine() : Version symfony ( inf ou égal à )  5  
+        $entityManager = $doctrine->getManager();
+        $personne = new Personne();
+        $personne->setFirstname('Jacques');
+        $personne->setName('Gourgandin');
+        $personne->setAge('34');
+
+        // Ajouter l'opération d'insetion de la personne dans ma transaction
+        $entityManager->persist($personne);
+
+        // Execute la transaction
+        $entityManager->flush();
+        return $this->render('personne/detail.html.twig', [
+            'personne' => $personne,
         ]);
     }
 }
